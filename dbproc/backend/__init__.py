79cbabf6c0498b0acbc634664ee177269e759f50
Wijnand Modderman-Lenstra <maze@pyth0n.org>
2014-02-24 00:02:19 +0100

    Not needed

--- dbproc/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-from dbproc.backend.base import Backend
-
-
-def for_connection(instance):
-    for cls in Backend.classes.values():
-        if cls.can_handle(instance):
-            return cls(instance)
